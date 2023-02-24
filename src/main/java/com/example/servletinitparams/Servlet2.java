c7b7c5efadb88b7c58f09ba68627e9287428bb74
moshaik1 <mrsrnz@umsystem.edu>
2023-02-23 20:38:09 -0600

    Initial commit

---
+++ src/main/java/com/example/servletinitparams/Servlet2.java
@@ -0,0 +1,18 @@
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "Servlet2", value = "/Servlet2")
+public class Servlet2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
